2d834e82184844d0b28c0691ffc679c2f346ff19
Mitchell <Hmitchell.hall@gmail.com>
2021-08-19 14:16:16 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const hq = 42
+
+const distanceFromHqInBlocks = (destBlock) =>{
+    if(hq > destBlock){
+        return hq - destBlock
+    }
+    else if(hq < destBlock){
+        return destBlock - hq
+    }
+}
+
+const distanceFromHqInFeet = (block) =>  distanceFromHqInBlocks(block) * 264
+
+
+const distanceTravelledInFeet = (start, end) => {
+    if(start > end){
+        return (start - end) * 264
+    }
+    else if(start < end){
+        return (end - start) * 264
+    }
+}
+
+const calculatesFarePrice = (start, destination) => {
+   const ride  = distanceTravelledInFeet(start, destination)
+    if (ride < 400){
+       return 0
+   }
+   else if(ride > 400 && ride <2000){
+       return (ride - 400) * .02
+   }
+   else if(ride > 2000 && ride < 2500){
+       return 25
+   }
+   else{ return 'cannot travel that far'}
+}
